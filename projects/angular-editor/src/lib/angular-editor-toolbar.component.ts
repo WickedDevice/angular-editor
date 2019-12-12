9f6022cbc5bbab7e4412c8d984d16e43fd211922
Joe Nolan <joseph.nolan@wickeddevice.com>
2019-12-12 14:00:26 -0500

    remove heading/paragraph picker;

--- projects/angular-editor/src/lib/angular-editor-toolbar.component.ts
+++ projects/angular-editor/src/lib/angular-editor-toolbar.component.ts
@@ -30,26 +30,6 @@ export class AngularEditorToolbarComponent {
       value: 'h2',
     },
     {
-      label: 'Heading 3',
-      value: 'h3',
-    },
-    {
-      label: 'Heading 4',
-      value: 'h4',
-    },
-    {
-      label: 'Heading 5',
-      value: 'h5',
-    },
-    {
-      label: 'Heading 6',
-      value: 'h6',
-    },
-    {
-      label: 'Heading 7',
-      value: 'h7',
-    },
-    {
       label: 'Paragraph',
       value: 'p',
     },
