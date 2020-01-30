c9b41e6994a6c242ac263c1ef2f901de7d5954ef
Joe Nolan <joseph.nolan@wickeddevice.com>
2020-01-30 14:32:08 -0500

    always paste as plain text;

--- projects/angular-editor/src/lib/angular-editor.component.ts
+++ projects/angular-editor/src/lib/angular-editor.component.ts
@@ -100,8 +100,34 @@ export class AngularEditorComponent implements OnInit, ControlValueAccessor, Aft
   }
 
   ngAfterViewInit() {
+    // Replace normal Paste with Paste Plain Text to simplify UX --JCN
+    this.textArea.nativeElement.addEventListener('paste', this.plainPaste.bind(this));
+
     if (isDefined(this.autoFocus)) {
       this.focus();
+    }
+  }
+
+  plainPaste(e: any) {
+    console.log();
+    e.preventDefault();
+    let text = '';
+
+    if (e.clipboardData && e.clipboardData.getData) {
+      text = e.clipboardData.getData('text/plain');
+      this.doc.execCommand('insertHTML', false, text);
+    } else if (this.doc.defaultView.clipboardData && this.doc.defaultView.clipboardData.getData) {
+      text = this.doc.defaultView.clipboardData.getData('Text');
+      if (this.doc.defaultView.getSelection) {
+        const sel = this.doc.defaultView.getSelection();
+        if (sel.getRangeAt && sel.rangeCount) {
+          const range = sel.getRangeAt(0);
+          range.deleteContents();
+          range.insertNode(this.doc.createTextNode(text));
+        }
+      } else if (this.doc.selection && this.doc.selection.createRange) {
+        this.doc.selection.createRange().text = text;
+      }
     }
   }
 
